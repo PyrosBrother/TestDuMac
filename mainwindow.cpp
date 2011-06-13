aea3b6e9d3c2ba14f5c18f9102968ca1b9bd824d
PyrosBrother <git.pyrosbrother@spamgourmet.net>
2011-06-14 00:14:16 +0200

    Réalisation de l'appel à la boite de dialogue A propos...

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "about.h"
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -11,4 +12,11 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_action_A_propos_triggered()
+{
+    DialogAbout *about = new DialogAbout (this);
+
+    about->exec();
 }
--- mainwindow.h
+++ mainwindow.h
@@ -15,6 +15,9 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+    void on_action_A_propos_triggered();
+
 private:
     Ui::MainWindow *ui;
 };
